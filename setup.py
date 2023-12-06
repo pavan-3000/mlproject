23a27c2730ab13efb930aab28cdabef8a00679fc
pavan-3000 <pavansingh3000@gmail.com>
2023-12-06 18:16:33 +0530

    requriments for projects

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mlpro",
+    version="0.0.0",
+    author="pavan",
+    author_email="pavansingh3000@gmail.com",
+    packages=find_packages(where='scr')
+)
